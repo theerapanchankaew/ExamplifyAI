a7e218b7831db4759d46441025e3db51663d2b08
Your Name <theerapanMASCI@gmail.com>
2025-10-13 07:45:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "masci-exam-systemc",
+  "appId": "1:292124549923:web:f4da2798ec5397bc592296",
+  "apiKey": "AIzaSyDoI0rXbEeWPrQoG7viy6HXKwU_kQp6Lvk",
+  "authDomain": "masci-exam-systemc.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "292124549923"
+};
