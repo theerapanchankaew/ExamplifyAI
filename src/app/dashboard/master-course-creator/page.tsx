caf04bd4fb7220e5ed60ef4b1cd0dc427276e896
Theerapan C <theerapanMASCI@gmail.com>
2025-10-15 09:10:40 +0000

    Try fixing this error: `Runtime Error: Error: Label is not defined. Erro

--- src/app/dashboard/master-course-creator/page.tsx
+++ src/app/dashboard/master-course-creator/page.tsx
@@ -15,6 +15,7 @@ import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '@/components/ui/card';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, FormDescription } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
